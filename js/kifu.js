81f4e168e958e895f259812f095c27c89d464ac0
12ange <flair.t9@gmail.com>
2019-03-19 20:01:12 +0900

    駒落ち戦の出力を正しく
    ( cf: commit# 2e734ca / ShogiGUIで確認 )

--- js/kifu.js
+++ js/kifu.js
@@ -22,9 +22,11 @@ const gcKifu = {
 				otosuStr.push(`${k.pos>>>4}${k.pos&15}${gcKifu.pieceStr[k.kind-1]}`);
 			}
 		}
+		//駒落ち戦なら、後手表示である上手が初手。そのため先手スキップを明示する行を仕込む
+		if(otosuStr.length>1){ otosuStr.push("\n-") }
 		//平手戦かつ_bComPlaysFirstの時は記録する駒位置を反転するフラグを立てる
 		gcKifu.flagTenchi = otosuStr.length===1 && _bComPlaysFirst;
-
+		gcKifu.isFinished = false;
 		gcKifu.arrLog.length=0;
 		gcKifu.log(
 			"V2.2",
@@ -33,8 +35,6 @@ const gcKifu = {
 			`$START_TIME:${gcKifu.generateTimeStr()}`,
 			otosuStr.join("")
 		);
-
-		gcKifu.isFinished = false;
 		if( gcKifu.lastObjectUrl !== "" ){
 			//Blobの解放
 			URL.revokeObjectURL(gcKifu.lastObjectUrl);
@@ -42,12 +42,10 @@ const gcKifu = {
 		}
 	},
 	move : function(_te,_teban,_komaId){
-
 		const t = v=>""+(gcKifu.flagTenchi?10-v:v);
 		let thisPiece = gPieces[_komaId],
 			strFrom = _teban^gcKifu.flagTenchi?"-":"+",
 			strPKind;
-
 		if(_te.isUtsu){
 			strPKind = gcKifu.pieceStr[thisPiece.kind-1];
 			strFrom += "00";
@@ -55,28 +53,24 @@ const gcKifu = {
 			strPKind = gcKifu.pieceStr[ thisPiece.kind+( thisPiece.isNari ? 7:-1 )];
 			strFrom += t(_te.fromSuji)+t(_te.fromDan);
 		}
-
-		gcKifu.log( strFrom+t(_te.toSuji)+t(_te.toDan)+strPKind);
+		gcKifu.log( strFrom+t(_te.toSuji)+t(_te.toDan)+strPKind );
 	},
 	finish : function(){
 		gcKifu.log("%TSUMI",`$END_TIME:${gcKifu.generateTimeStr()}`)
 		gcKifu.isFinished = true;
 	},
 	getCSAFileAs : function(_filename="komao",_flush=false){
-
 		if( _flush && gcKifu.lastObjectUrl !== "" ){
 			//Blobの解放
 			URL.revokeObjectURL(gcKifu.lastObjectUrl);
 			gcKifu.lastObjectUrl = "";
 		}
-
 		if( gcKifu.lastObjectUrl === "" ){
 			//String -> Blob -> ObjectURL
 			let s = gcKifu.arrLog.join("\n")+(gcKifu.isFinished?"":"\n%CHUDAN");
 			let bb = new Blob([s],{type:"text/plain"});
 			gcKifu.lastObjectUrl = URL.createObjectURL(bb);
 		}
-
 		let a = document.createElement("a");
 		a.href = gcKifu.lastObjectUrl;
 		a.target = "_blank";
